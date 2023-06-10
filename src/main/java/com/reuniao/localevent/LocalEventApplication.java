2b6f16101ee7116ae8beaf2c0b98421136221f1e
evertonantonio <101748157+evertonantonio@users.noreply.github.com>
2023-06-10 07:19:30 -0300

    alteração na main

--- src/main/java/com/reuniao/localevent/LocalEventApplication.java
+++ src/main/java/com/reuniao/localevent/LocalEventApplication.java
@@ -4,8 +4,8 @@ import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 
 @SpringBootApplication
-public class LocaleventApplication {
+public class LocalEventApplication {
 
-	public static void main(String[] args) {SpringApplication.run(LocaleventApplication.class, args);}
+    public static void main(String[] args) {SpringApplication.run(LocalEventApplication.class, args);}
 
 }
