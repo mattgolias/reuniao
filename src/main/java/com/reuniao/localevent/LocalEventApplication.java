577d8a7b0d9eb3ffd6c60b7cd2e3d3b0c5e73865
evertonantonio <101748157+evertonantonio@users.noreply.github.com>
2023-06-09 23:54:07 -0300

    main

--- src/main/java/com/reuniao/localevent/LocalEventApplication.java
+++ src/main/java/com/reuniao/localevent/LocalEventApplication.java
@@ -2,8 +2,9 @@ package com.reuniao.localevent;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.boot.autoconfigure.jdbc.DataSourceAutoConfiguration;
 
-@SpringBootApplication
+@SpringBootApplication(exclude={DataSourceAutoConfiguration.class})
 public class LocalEventApplication {
 
 	public static void main(String[] args) {
